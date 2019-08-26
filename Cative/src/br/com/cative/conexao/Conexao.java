2ef781099dfcc241e8aa1cb71cb9b89033910cfd
Logon Aluno <logonaluno@local.com>
2019-08-26 20:43:09 -0300

    adiciona Conexao

--- Cative/src/br/com/cative/conexao/Conexao.java
+++ Cative/src/br/com/cative/conexao/Conexao.java
@@ -1,5 +1,13 @@
 package br.com.cative.conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Conexao {
+	public static Connection getConexao() throws Exception{
+			return DriverManager.getConnection
+					("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL","RM83888","fiap");
+	}
 	
-}
+	}
+	
+
