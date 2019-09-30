9cc4d84b48fac13880dec866ad7ba4e651246020
Logon Aluno <logonaluno@local.com>
2019-09-30 19:59:31 -0300

    Atualiza Teste add aluno em turma

---
+++ cative/src/br/com/cative/teste/TesteAdicionarAlunoEmTurma.java
@@ -0,0 +1,26 @@
+package br.com.cative.teste;
+
+import javax.swing.JOptionPane;
+
+import br.com.cative.beans.Turma;
+import br.com.cative.beans.Usuario;
+import br.com.cative.dao.TurmaDAO;
+
+public class TesteAdicionarAlunoEmTurma {
+
+	public static void main(String[] args) {
+		try {
+			Turma t = new Turma();
+			t.setIdTurma(1);
+			Usuario u = new Usuario();
+			u.setIdUsuario(Integer.parseInt(JOptionPane.showInputDialog("Digite o id do aluno")));
+			TurmaDAO dao = new TurmaDAO();
+			dao.addAluno(u.getIdUsuario());
+			System.out.println("Aluno adicionado com sucesso");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
