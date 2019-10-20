cd318119e7b65979532f0179e7b51fee3485f86e
azabraao <az.abraao@gmail.com>
2019-10-20 13:05:48 -0300

    Modifica teste de conexão

--- cative/src/br/com/cative/teste/TesteConexao.java
+++ cative/src/br/com/cative/teste/TesteConexao.java
@@ -1,26 +1,17 @@
 package br.com.cative.teste;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+import br.com.cative.dao.*;
 
 
 public class TesteConexao {
 	public static void main(String[] args) throws SQLException { 
 		try {
-			Connection connection = null;
-			String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-			connection = DriverManager.getConnection(url , "rm83888" , "fiap");
-			System.out.println("Abriu a conexão");
-			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from MISSÃO");
-			while(rs.next()) {
-				System.out.println(rs.getString("ID_MISSAO"));
-				System.out.println(rs.getString("OBJETIVO"));
-			}
+			UsuarioDAO dao = new UsuarioDAO();
+			System.out.println("Conectou?");
 		} catch(Exception e) {
+			System.out.println("Conectou n");
 			e.printStackTrace();
 		}
 	}
