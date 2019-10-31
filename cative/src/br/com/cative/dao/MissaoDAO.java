be0f56c8a4f879e45384d5f82c5acc67eef7a799
azabraao <az.abraao@gmail.com>
2019-10-31 16:19:27 -0300

    Adiciona filtro para missão concluida

--- cative/src/br/com/cative/dao/MissaoDAO.java
+++ cative/src/br/com/cative/dao/MissaoDAO.java
@@ -85,8 +85,11 @@ public class MissaoDAO {
 	 *   */
 	public List getMissoes(int idUsuario) throws Exception{
 		stmt = con.prepareStatement
-				("select * from tb_missao as m join tb_turma_has_tb_missao as tt on m.id_missao = tt.tb_missao_id_missao join tb_turma as t on t.id_turma = tt.tb_turma_id_turma join tb_usuario_has_tb_turma as tu on t.id_turma = tu.tb_turma_id_turma join tb_usuario as u on u.id_usuario = tu.tb_usuario_id_usuario where id_usuario = ?;\r\n" + 
-						"");
+		("select * from tb_missao as m join tb_turma_has_tb_missao as tt on m.id_missao = tt.tb_missao_id_missao "
+				+ "join tb_turma as t on t.id_turma = tt.tb_turma_id_turma "
+				+ "join tb_usuario_has_tb_turma as tu on t.id_turma = tu.tb_turma_id_turma "
+				+ "join tb_usuario as u on u.id_usuario = tu.tb_usuario_id_usuario "
+				+ "where id_usuario = ? and missao_concluida = 0");
 		stmt.setInt(1, idUsuario);
 		rs = stmt.executeQuery();
 		List<Missao> missoes = new ArrayList<Missao>();
